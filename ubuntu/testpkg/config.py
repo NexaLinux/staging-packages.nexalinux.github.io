c29d3a29af6d14ca986c2848ffd218f14164fc32
komaru.yml <170887716+komaru-meow@users.noreply.github.com>
2025-04-01 15:43:39 +0200

    [packages] [ubuntu] [testpkg] This is why you don't use GitHub web UI :sob:

--- ubuntu/testpkg/config.py
+++ ubuntu/testpkg/config.py
@@ -3,6 +3,6 @@ maintainer_email = "winbo.yml@proton.me"
 version = 1.0
 dependencies = []
 steps = [
-  echo "Hello, world!",
-  echo "Goodbye, world!"
+  'echo "Hello, world!"',
+  'echo "Goodbye, world!"'
 ]
