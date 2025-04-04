d19c22eda8d7510a3c1e481b11245d8027f140c6
komaru.yml <170887716+komaru-meow@users.noreply.github.com>
2025-04-04 18:28:02 +0200

    [staging-packages] [ubuntu] [spicetify] [config.py] Convert testpkg to Spicetify

---
+++ ubuntu/spicetify/config.py
@@ -0,0 +1,12 @@
+maintainer = "komaru.yml"
+maintainer_email = "winbo.yml@proton.me"
+version = "N/A"
+dependencies = ["curl", "dash"]
+steps = [
+  'curl -fsSL https://raw.githubusercontent.com/spicetify/cli/main/install.sh | sh',
+  '~/.spicetify/spicetify backup apply'
+]
+uninstall_stepps = [
+  'spicetify restore',
+  'rm -rf ~/.spicetify'
+]
--- ubuntu/testpkg/config.py
+++
@@ -1,8 +0,0 @@
-maintainer = "komaru.yml"
-maintainer_email = "winbo.yml@proton.me"
-version = 1.0
-dependencies = []
-steps = [
-  'echo "Hello, world!"',
-  'echo "This is a demo of nexa-pkg, the package manager coming to Nexa Linux."'
-]
